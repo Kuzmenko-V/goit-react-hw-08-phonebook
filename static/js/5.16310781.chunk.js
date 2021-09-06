5e553703e2ec81db0ce000bcbd5d9982e08cbbbe
Kuzmeko Vladimir <wmeywise@gmail.com>
2021-09-06 11:34:37 +0300

    Updates

---
+++ static/js/5.16310781.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-08-phonebook"]=this["webpackJsonpgoit-react-hw-08-phonebook"]||[]).push([[5],{177:function(t,e,n){"use strict";n.r(e);n(0);var i=n(147),o=n(2),c={title:{fontWeight:500,fontSize:48,textAlign:"center"}};e.default=function(){return Object(o.jsx)(i.a,{children:Object(o.jsx)("h1",{style:c.title,children:"\u0414\u043e\u0431\u0440\u043e \u043f\u043e\u0436\u0430\u043b\u043e\u0432\u0430\u0442\u044c \u043d\u0430 \u043d\u0430\u0448 \u0441\u0435\u0440\u0432\u0438\u0441!"})})}}}]);
+//# sourceMappingURL=5.16310781.chunk.js.map
